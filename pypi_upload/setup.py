ed660cdeae531d79ab50324b8aff156835d239df
madman-bob <madman.bob@hotmail.co.uk>
2020-05-04 14:42:58 +0100

    Replace Travis with CircleCI

--- pypi_upload/setup.py
+++ pypi_upload/setup.py
@@ -1,26 +1,59 @@
-from setuptools import setup, find_packages
-from os import path
-
+import os
 import re
+import sys
+from pathlib import Path
 
-project_root = path.join(path.abspath(path.dirname(__file__)), '..')
+from setuptools import find_packages, setup
+from setuptools.command.install import install
+
+project_root = Path(__file__).parents[1]
+
+
+class VerifyCommand(install):
+    """Custom command to verify module integrity"""
+
+    description = "verify module integrity"
+
+    @staticmethod
+    def verify_git_tag():
+        tag = os.getenv("CIRCLE_TAG")
+        version = get_version()
+
+        if tag != version:
+            sys.exit(
+                "Git tag: {0} does not match the version of this app: {1}".format(
+                    tag, version
+                )
+            )
+
+    def run(self):
+        self.verify_git_tag()
 
 
 def get_version():
-    with open(path.join(project_root, 'argparse_utils', '__init__.py'), encoding='utf-8') as init_file:
-        return re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", init_file.read(), re.M).group(1)
+    init_contents = (project_root / "argparse_utils/__init__.py").read_text(encoding="utf-8")
+
+    return re.search(r"^__version__ = ['\"]([^'\"]*)['\"]", init_contents, re.M).group(
+        1
+    )
+
+
+def get_requirements():
+    requirements = (project_root / "requirements.txt").read_text(encoding="utf-8")
+
+    return list(filter(None, requirements.splitlines()))
 
 
 def get_long_description():
-    with open(path.join(project_root, 'README.md'), encoding='utf-8') as readme_file:
-        return readme_file.read()
+    return (project_root / "README.md").read_text(encoding="utf-8")
 
 
 setup(
     name='argparse_utils',
     version=get_version(),
     packages=find_packages(include=('argparse_utils', 'argparse_utils.*')),
-    install_requires=[],
+    install_requires=get_requirements(),
+    setup_requires=["wheel"],
 
     author='Robert Wright',
     author_email='madman.bob@hotmail.co.uk',
@@ -32,10 +65,14 @@ setup(
     license='MIT',
     classifiers=[
         'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7'
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
     ],
-    python_requires='>=2.7'
+    python_requires='>=2.7',
+    cmdclass={"verify": VerifyCommand},
 )
