c2956b2d01e48b311229b20db48d7218f0217d85
madman-bob <madman.bob@hotmail.co.uk>
2020-05-04 17:20:02 +0100

    Remove claims of Python 2.7 support

--- pypi_upload/setup.py
+++ pypi_upload/setup.py
@@ -65,14 +65,12 @@ setup(
     license='MIT',
     classifiers=[
         'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
     ],
-    python_requires='>=2.7',
+    python_requires='>=3.5',
     cmdclass={"verify": VerifyCommand},
 )
